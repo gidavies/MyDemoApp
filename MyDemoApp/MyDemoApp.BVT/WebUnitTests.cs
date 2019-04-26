d65b7d7d5bb35ee833e2578af0f587c35fab082d
Giles Davies <giles.j.davies@hotmail.com>
2019-04-26 22:09:50 +0100

    Add unit tests

---
+++ MyDemoApp/MyDemoApp.BVT/WebUnitTests.cs
@@ -0,0 +1,45 @@
+using MyDemoApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyDemoApp.Web.Models;
+
+namespace MyDemoApp.BVT
+{
+    [TestClass]
+    public class WebUnitTests
+    {
+        [TestMethod]
+        public void HomePageTest()
+        {
+            var controller = new HomeController();
+            IActionResult result = controller.Index();
+            Assert.AreEqual(null, controller.ViewData["Message"]);
+        }
+
+        [TestMethod]
+        public void PrivacyPageTest()
+        {
+            var controller = new HomeController();
+            IActionResult result = controller.Privacy();
+            Assert.AreEqual(null, controller.ViewData["Message"]);
+        }
+
+        [TestMethod]
+        public void ColoursPageTest()
+        {
+            var controller = new HomeController();
+            var colourModel = new ColoursModel();
+            IActionResult result = controller.Colours(colourModel);
+            Assert.AreEqual(null, controller.ViewData["Message"]);
+        }
+
+        [TestMethod]
+        public void MessagingPageTest()
+        {
+            var controller = new HomeController();
+            var messagingModel = new MessagingModel();
+            IActionResult result = controller.Messaging(messagingModel);
+            Assert.AreEqual(null, controller.ViewData["Message"]);
+        }
+    }
+}
